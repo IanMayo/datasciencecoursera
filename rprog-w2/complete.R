e3064a6f8cc653a4b456ce54deff829087c82788
Ian Mayo <ian@planetmayo.com>
2014-06-10 15:48:11 +0100

    part way complete

--- rprog-w2/complete.R
+++ rprog-w2/complete.R
@@ -12,4 +12,27 @@ complete <- function(directory, id = 1:332) {
     ## ...
     ## where 'id' is the monitor ID number and 'nobs' is the
     ## number of complete cases
+    
+    ## declare the placeholder for the new means
+    fullList <- c()
+    
+    ## loop through the id numbers
+    for(i in 1)
+    {
+        ## sort out the file name
+        fName <- paste(sprintf("%03d", i),".csv", sep="")
+        
+        ## and the path
+        fPath <- paste(directory,"/",fName,sep="")
+        
+        ## open the relevant file
+        thisD <- read.csv(fPath)
+        
+        ## get the list of complete cases        
+        valid <- thisD[thisD,]
+    }  
+    
+    ## return the result
+    valid
+    
 }
