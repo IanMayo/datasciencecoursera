a563d24db3a5d9de2ca11d42d585d80e616dfb26
Ian Mayo <ian@planetmayo.com>
2014-07-09 16:27:20 +0100

    simplify labels

--- explore_data_proj2/proj1.R
+++ explore_data_proj2/proj1.R
@@ -2,18 +2,17 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-## work out the totals, year
-totalEmi = aggregate(Emissions ~ year, data=NEI, sum)
+## work out the totals for each year
+data = aggregate(Emissions ~ year, data=NEI, sum)
 
 ## declare the output image
 png(filename="plot1.png", width=480, height=480, units="px")
 
 ## draw the plot
-plot(totalEmi$Emissions, type="b", 
+plot(data$Emissions, type="l", 
      xaxt="n",  xlab="Year",
-##     ylab=expression(PM[2.5]~Emissions~(`in`~tons)), 
-     ylab="PM2.5 Emissions (tons))", 
-     main=expression(Total~PM[2.5]~Emissions~`in`~United~States))
+     main="PM2.5 Emissions in USA",
+     ylab="Annual Total PM2.5 Emissions (tons)")
 
 ## stick in the correct labels
 axis(1, at=c(1:4), labels=c("1999", "2002", "2005", "2008"))
