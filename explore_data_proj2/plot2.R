8b5c957aa736619f776a2b5ade9dc6a2217cf14f
Ian Mayo <ian@planetmayo.com>
2014-07-15 12:56:05 +0100

    step two answers

---
+++ explore_data_proj2/plot2.R
@@ -0,0 +1,20 @@
+## 1. load the file
+NEI <- readRDS("summarySCC_PM25.rds")
+
+## 2. Get the baltimore data
+data <- NEI[NEI$fips=="24510",]
+
+## 3. Transform the data
+data <- aggregate(Emissions ~ year, data, sum)
+
+## 4. Prepare the output file
+png('plot2.png')
+
+## 5. Produce the plot
+barplot(height=data$Emissions,
+        names.arg=data$year,
+        xlab="years", ylab="Annual PM2.5 emissions",
+        main='Total PM2.5 emissions in Baltimore')
+
+## 6. Output the plot
+dev.off()
