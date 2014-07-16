6d4aadc9ea1f2cfa19be5471e549d83633a45ee2
Ian Mayo <ian@planetmayo.com>
2014-07-16 14:08:29 +0100

    modify solution so that it actually uses the CODES file

--- explore_data_proj2/plot5.R
+++ explore_data_proj2/plot5.R
@@ -5,21 +5,24 @@ library(ggplot2)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-## 3. Filter the Baltimore vehicle emissions
-bmoreData <- NEI[(NEI$type=="ON-ROAD") & (NEI$fips=="24510"),]
-bmoreAggr <- aggregate(Emissions ~ year, data=bmoreData, FUN=sum)
+## 3. filter the data
+vehicleCategories <- SCC$SCC[grep("Veh",SCC$Short.Name)]  ## find all vehicle related codes
+vehicleEmissions <- NEI$SCC %in% vehicleCategories         ## find observations related to these
+baltimoreData <- NEI$fips==24510                           ## find baltimore observations
 
-## 4. Prepare the output file
+## 4. aggregate the two datasets
+sources<-aggregate(NEI$Emissions[vehicleEmissions & baltimoreData] ~ NEI$year[vehicleEmissions & baltimoreData] , NEI, sum)
+
+## 5. annotate the data axes
+colnames(sources)<-c("Year","Emissions")
+
+## 6. Prepare the output file
 png("plot5.png", height=480, width=600)
 
-## 5. Produce the plot
-p <- ggplot(bmoreAggr, aes(x=factor(year), y=Emissions)) +
-    geom_bar(stat="identity") +
-    xlab("Year") +
-    ylab("Total PM2.5 emissions (Tons)") +
-    ggtitle("Baltimore Vehicle Emissions")
+## 7. Produce the plot
+p <- ggplot(sources, aes(x=factor(Year),y=Emissions)) + geom_bar(stat = "identity") +
+    ggtitle("Baltimore City Vehicle Emissions") + labs(x = "Years")
 
-## 6. Output the plot
+## 8. Output the plot
 print(p)
 dev.off()
-
