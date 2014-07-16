f419fdc3715f8374352a6b456ebefbbe1134b747
Ian Mayo <ian@planetmayo.com>
2014-07-16 14:21:40 +0100

    consistent variable naming

--- explore_data_proj2/plot5.R
+++ explore_data_proj2/plot5.R
@@ -11,16 +11,16 @@ vehicleEmissions <- NEI$SCC %in% vehicleCategories         ## find observations 
 baltimoreData <- NEI$fips==24510                           ## find baltimore observations
 
 ## 4. aggregate the two datasets
-sources<-aggregate(NEI$Emissions[vehicleEmissions & baltimoreData] ~ NEI$year[vehicleEmissions & baltimoreData] , NEI, sum)
+data<-aggregate(NEI$Emissions[vehicleEmissions & baltimoreData] ~ NEI$year[vehicleEmissions & baltimoreData] , NEI, sum)
 
 ## 5. annotate the data axes
-colnames(sources)<-c("Year","Emissions")
+colnames(data)<-c("Year","Emissions")
 
 ## 6. Prepare the output file
 png("plot5.png", height=480, width=600)
 
 ## 7. Produce the plot
-p <- ggplot(sources, aes(x=factor(Year),y=Emissions)) + geom_bar(stat = "identity") +
+p <- ggplot(data, aes(x=factor(Year),y=Emissions)) + geom_bar(stat = "identity") +
     ggtitle("Baltimore City Vehicle Emissions") + labs(x = "Years")
 
 ## 8. Output the plot
